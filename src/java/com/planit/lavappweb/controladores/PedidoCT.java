2c53979749b8da80529b707bc54ec39b8896607b
johanncanon <johanncanon@gmail.com>
2016-07-09 12:30:19 -0500

    arreglo de plataforma de administrador, ingreso de librerias de bootsfaces actualizada e ingreso de estilos de plantila de SMSRenta

--- src/java/com/planit/lavappweb/controladores/PedidoCT.java
+++ src/java/com/planit/lavappweb/controladores/PedidoCT.java
@@ -5,14 +5,26 @@
  */
 package com.planit.lavappweb.controladores;
 
+import com.planit.lavappweb.modelos.Pedido_TO;
+
 /**
  *
  * @author Desarrollo_Planit
  */
 public class PedidoCT {
 
-    
+    private Pedido_TO pedido;
+
     public PedidoCT() {
+        pedido = new Pedido_TO();
     }
-    
+
+    public Pedido_TO getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido_TO pedido) {
+        this.pedido = pedido;
+    }
+
 }
